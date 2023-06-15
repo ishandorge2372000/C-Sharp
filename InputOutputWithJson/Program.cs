1f892ea5316cc9e6f3683aef82c4c7742ea2d091
ishandorge2372000 <80149555+ishandorge2372000@users.noreply.github.com>
2023-06-16 04:02:26 +0530

    Reading JSON through C#

---
+++ InputOutputWithJson/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+using Newtonsoft.Json;
+
+namespace InputOutputWithJson
+{
+    public class Program
+    {
+        private const string jsonFile = "Student.json";
+        static void Main(string[] args)
+        {
+            //string studentData = ReadStudentData();
+            //Console.WriteLine(studentData);
+            var studentData = ReadStudentDataUsingDeserializer();
+            ShowStudentData(studentData);
+            Console.ReadLine();
+        }
+
+        public static string ReadStudentData()
+        {
+            string studentData = System.IO.File.ReadAllText(jsonFile);
+            return studentData;
+        }
+
+        public static IList<Student> ReadStudentDataUsingDeserializer()
+        {
+            var studentData = System.IO.File.ReadAllText(jsonFile);
+            return JsonConvert.DeserializeObject <IList<Student>>(studentData);
+        }
+
+        public static void ShowStudentData(IList<Student> studentData)
+        {
+            foreach (var student in studentData)
+            {
+                Console.WriteLine($"Student First Name : {student.firstName}\nStudent First Name : {student.lastName}\nAge : {student.age}\n\n");
+            }
+        }
+    }
+}
